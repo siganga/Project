6a9ef5692f2fb4e1496fe0b766b5dda233f9ec54
thesharkfire <itachiuchiha902316@gmail.com>
2025-10-21 15:11:24 +0300

    final 3

--- client/src/pages/secondary-pages/Reports.jsx
+++ client/src/pages/secondary-pages/Reports.jsx
@@ -147,7 +147,7 @@ function Reports() {
           onClick={downloadReport}
           className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
         >
-          Download PDF Report 📄
+          Download Report ⬇️
         </button>
 
         <button
